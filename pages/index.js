b87e6f647884ac8deedf02c90c58118cbf3aa5dc
kutewhiz <>
2021-10-03 03:16:56 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+function HomePage() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <h1 style={{ fontFamily: 'fantasy', color: '#000000' }}>
+        snowfox's website
+      </h1>
+      <h2
+        style={{
+          fontFamily: 'cursive',
+          color: 'gold',
+          marginBottom: '10rem'
+        }}
+      >
+        click the buttons below
+      </h2>
+      <button
+        style={{
+          fontFamily: 'monospace',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: 'black',
+          color: 'white'
+        }}
+        onClick={() => alert('Hello there')}
+      >
+        Welcome
+      </button>
+      <button
+        style={{
+          fontFamily: 'monospace',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: 'gold',
+          color: 'black',
+          marginTop: '2rem'
+        }}
+        onClick={() => {
+          let name = prompt("What's your name?");
+          if (name) {
+            alert('Nice to meet you, ' + name + '!');
+          } else {
+            alert('Nice to meet you, stranger');
+          }
+        }}
+      >
+        {"What's your name?"}
+      </button>
+    </div>
+  );
+}
+
+export default HomePage;
